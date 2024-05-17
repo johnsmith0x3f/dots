e7de89bc8df6c9e114251cf69660ac6385e8226c
johnsmith0x3f <johnsmith0x3f@gmail.com>
2024-05-17 06:23:02 +0000

    Move options into options.lua.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,4 @@
 -- print("Hello Neovim!")
 
--- Enable line number.
-vim.opt.number = true
--- Use relative number for better hopping around.
-vim.opt.relativenumber = true
-
--- Enable cursorline highlight.
-vim.opt.cursorline = true
-
--- Set the width of tab characters...
-vim.opt.tabstop = 4
--- and the width of indentations to always be the same.
-vim.opt.shiftwidth = 0
--- Use spaces instead of tabs when indenting.
-vim.opt.expandtab = true
+-- Load configs.
+require("john/configs")
