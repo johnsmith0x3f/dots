63e26c4d4edf18231c8fe8fbb1927aacc5517a65
johnsmith0x3f <johnsmith0x3f@gmail.com>
2024-05-17 06:39:57 +0000

    Add keymaps.lua.

---
+++ nvim/lua/john/keymaps.lua
@@ -0,0 +1,29 @@
+-- Use comma as leader key.
+-- Because '\' is too far away.
+vim.g.mapleader = ","
+
+local set = vim.keymap.set
+local opt = { noremap = true, silent = true }
+
+-- Prevent bad habits like using the arrow keys for movement.
+-- In normal mode...
+set("n", "<Left>",  ':echo "Use h"<CR>', opt)
+set("n", "<Down>",  ':echo "Use j"<CR>', opt)
+set("n", "<Up>",    ':echo "Use k"<CR>', opt)
+set("n", "<Right>", ':echo "Use l"<CR>', opt)
+-- and in insert mode.
+set("i", "<Left>",  '<Esc>:echo "Use h"<CR>', opt)
+set("i", "<Down>",  '<Esc>:echo "Use j"<CR>', opt)
+set("i", "<Up>",    '<Esc>:echo "Use k"<CR>', opt)
+set("i", "<Right>", '<Esc>:echo "Use l"<CR>', opt)
+
+-- Better window navigations.
+-- Whenever you can use hjkl, use them.
+set("n", "<C-h>", "<C-w>h", opt)
+set("n", "<C-j>", "<C-w>j", opt)
+set("n", "<C-k>", "<C-w>k", opt)
+set("n", "<C-l>", "<C-w>l", opt)
+
+-- Better indenting.
+set("v", "<", "<gv", opt)
+set("v", ">", ">gv", opt)
