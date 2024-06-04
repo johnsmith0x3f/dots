6b762a0acb185037ec4fd44b6585b346fb6cd627
johnsmith0x3f <johnsmith0x3f@gmail.com>
2024-06-04 02:27:32 +0000

    Update lualine & telescope.

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,15 +1,16 @@
 return {
-    'nvim-lualine/lualine.nvim', name = "lualine",
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    name = "LuaLine", "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+
     config = function()
-        require('lualine').setup({
+        require("lualine").setup({
             sections = {
-                lualine_a = {'mode'},
-                lualine_b = {'branch', 'diff', 'diagnostics'},
-                lualine_c = {'filename'},
-                lualine_x = {'filetype'},
-                lualine_y = {'progress'},
-                lualine_z = {'location'}
+                lualine_a = { "", "mode" },
+                lualine_b = { "branch", "diff", "diagnostics" },
+                lualine_c = { "filename" },
+                lualine_x = { "filetype" },
+                lualine_y = { "progress" },
+                lualine_z = { "location" }
             }
         })
     end
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,8 +1,9 @@
 return {
-    "nvim-telescope/telescope.nvim", name = "telescope",
+    name = "Telescope", "nvim-telescope/telescope.nvim",
     dependencies = { "plenary" },
     keys = {
-        { mode = "n", "<Leader>ff", ":Telescope find_files hidden=true<CR>" }
+        { mode = "n", "<Leader>fa", ":Telescope find_files hidden=true<CR>" },
+        { mode = "n", "<Leader>ff", ":Telescope find_files<CR>" }
     },
     config = function()
         require("telescope").setup({})
