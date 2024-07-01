c17cdedc7f8f901ed557881f13138228a1d9f144
johnsmith0x3f <johnsmith0x3f@gmail.com>
2024-07-01 16:20:44 +0000

    Update awesome-wm config.

--- awesome/helpers.lua
+++ awesome/helpers.lua
@@ -3,6 +3,14 @@ local gears = require("gears")
 
 local helpers = {}
 
+helpers.colorizeText = function(txt, fg)
+	if fg == "" then
+		fg = "#ffffff"
+	end
+
+	return "<span foreground='" .. fg .. "'>" .. txt .. "</span>"
+end
+
 helpers.rrect = function(radius)
 	radius = radius or dpi(4)
 	return function(cr, width, height)
@@ -17,14 +25,6 @@ helpers.addHover = function(element, bg, hbg)
 	element:connect_signal('mouse::leave', function(self)
 		self.bg = bg
 	end)
-end
-
-helpers.colorizeText = function(txt, fg)
-	if fg == "" then
-		fg = "#ffffff"
-	end
-
-	return "<span foreground='" .. fg .. "'>" .. txt .. "</span>"
 end
 
 return helpers
