661a726da79df65cce395b43cb78dd1fe4a38f1f
johnsmith0x3f <johnsmith0x3f@gmail.com>
2024-07-02 02:43:03 +0000

    Update awesome-wm config.

--- awesome/theme/init.lua
+++ awesome/theme/init.lua
@@ -1,18 +1,18 @@
 -- Theme handling library.
 local beautiful = require("beautiful")
 -- Standard awesome library.
-local gears     = require("gears")
+local gears = require("gears")
 
 -- Themes define colors, icons, font and wallpapers.
 
-local theme       = {}
-theme.wallpaper   = gears.filesystem.get_configuration_dir() .. "theme/wall.png"
+local theme = {}
+theme.wallpaper = gears.filesystem.get_configuration_dir() .. "theme/wall.png"
 
 -- Set default font options.
-theme.font        = "Lexend"
-theme.sans        = "Lexend"
-theme.mono        = "Iosevka Nerd Font"
-
+theme.font = "Lexend"
+theme.sans = "Lexend"
+theme.mono = "Iosevka Nerd Font"
+-- Set colors.
 theme.bg          = "#181818"
 theme.altbg       = "#212121"
 theme.fg          = "#F8F8F8"
--- awesome/ui/wibar/module/time.lua
+++ awesome/ui/wibar/module/time.lua
@@ -5,7 +5,7 @@ local wibox = require("wibox")
 return wibox.widget {
 	{
 		font = beautiful.sans .. "Light 15",
-		format = "<b>" .. helpers.colorizeText("%H", beautiful.blue) .. "</b> : " .. helpers.colorizeText("%M", beautiful.fg),
+		format = "<b>" .. helpers.colorizeText("%H", beautiful.blue) .. " : " .. helpers.colorizeText("%M", beautiful.fg) .. "</b>",
 		widget = wibox.widget.textclock,
 	},
 	widget = wibox.container.place,
