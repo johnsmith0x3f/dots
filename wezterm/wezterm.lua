cd14c696d008a3bdb7247f30d9da78beb94ef928
johnsmith0x3f <johnsmith0x3f@gmail.com>
2024-07-02 10:18:52 +0000

    Update wezterm config.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,8 +7,15 @@ config.color_scheme = "Tokyo Night Storm"
 
 config.enable_tab_bar = false
 
+config.window_padding = {
+	left = "32px",
+	right = "32px",
+	top = "32px",
+	bottom = "32px"
+}
+
 -- Fonts.
 config.font = wezterm.font("OperatorMono Nerd Font Mono")
-config.font_size = 18
+config.font_size = 15
 
 return config
