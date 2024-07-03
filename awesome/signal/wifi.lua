64e6f4476e35d35b49b0fa8a0718976ccf2eba81
johnsmith0x3f <johnsmith0x3f@gmail.com>
2024-07-03 02:04:34 +0000

    Update awesome-wm config.

--- awesome/signal/wifi.lua
+++ awesome/signal/wifi.lua
@@ -1,5 +1,5 @@
-local awful = require('awful')
-local gears = require('gears')
+local awful = require("awful")
+local gears = require("gears")
 
 -- Network Fetching and Signal Emitting
 ---------------------------------------
@@ -7,8 +7,9 @@ local function emit_network_status()
 	awful.spawn.easy_async_with_shell(
 		"bash -c 'nmcli networking connectivity check'", function(stdout)
 			local status = not stdout:match("none") -- boolean
-			awesome.emit_signal('signal::network', status)
-		end)
+			awesome.emit_signal("signal::network", status)
+		end
+	)
 end
 
 -- Refreshing
