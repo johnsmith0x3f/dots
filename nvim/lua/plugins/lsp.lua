6e35678dd5ba0c6c576baa82def22d783c3a944e
johnsmith0x3f <johnsmith0x3f@gmail.com>
2024-07-08 11:54:31 +0000

    Update neovim LSP config.

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,36 +1,23 @@
 return {
-    {
-        "williamboman/mason.nvim", name = "mason",
-        config = function()
-            require("mason").setup({})
-        end
-    },
-    {
-        "williamboman/mason-lspconfig.nvim", name = "mason-lspconfig",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "bashls", "clangd", "vimls" }
-            })
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            require("lspconfig").bashls.setup({})
-            require("lspconfig").clangd.setup({})
-            require("lspconfig").lua_ls.setup({
-                settings = {
-                    Lua = {
-                        diagnostics = {
-                            -- Get the language server to recognize the `vim` global.
-                            globals = {
-                                "vim",
-                                "require"
-                            }
-                        }
-                    }
-                }
-            })
-        end,
-    }
+	{
+		"williamboman/mason.nvim", name = "mason",
+		config = function()
+			require("mason").setup({})
+		end
+	},
+	{
+		"williamboman/mason-lspconfig.nvim", name = "mason-lspconfig",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "bashls", "clangd", "vimls" }
+			})
+		end
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("lspconfig").bashls.setup({})
+			require("lspconfig").clangd.setup({})
+		end,
+	}
 }
