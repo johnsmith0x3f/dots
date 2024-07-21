c481a4eee6c79eb5a5918e071a898768d09fa7a4
johnsmith0x3f <johnsmith0x3f@gmail.com>
2024-07-21 03:15:08 +0000

    Update neovim config.

--- nvim/snippets/luasnip/tex.lua
+++ nvim/snippets/luasnip/tex.lua
@@ -1,10 +1,69 @@
-return {
-    -- Greek letters.
-    s({ trig = ";a", snippetType = "autosnippet" }, { t("\\alpha") }),
-    s({ trig = ";b", snippetType = "autosnippet" }, { t("\\beta") }),
-    s({ trig = ";g", snippetType = "autosnippet" }, { t("\\gamma") }),
-    s({ trig = ";e", snippetType = "autosnippet" }, { t("\\epsilon") }),
+local M = {
+	-- Greek letters.
+	s({ trig = ";a", snippetType = "autosnippet" },
+		{
+			t("\\alpha")
+		}),
+	s({ trig = ";b", snippetType = "autosnippet" },
+		{
+			t("\\beta")
+		}),
+	s({ trig = ";g", snippetType = "autosnippet" },
+		{
+			t("\\gamma")
+		}),
+	s({ trig = ";d", snippetType = "autosnippet" },
+		{
+			t("\\delta")
+		}),
+	s({ trig = ";e", snippetType = "autosnippet" },
+		{
+			t("\\epsilon")
+		}),
 
-    -- Text styles.
-    s("tt", fmta("\\texttt{<>}", { i(1) })),
+	-- Text styles.
+	s("tt",
+		fmta(
+			"\\texttt{<>}",
+			{
+				i(1)
+			}
+		)
+	),
+	s("mr",
+		fmta(
+			"\\mathrm{<>}",
+			{
+				i(1)
+			}
+		)
+	),
+
+	s("ff",
+		fmta(
+			"\\frac{<>}{<>}",
+			{
+				i(1),
+				i(2)
+			}
+		)
+	),
+	
+	s({ trig="env", snippetType="autosnippet" },
+		fmta(
+			[[
+				\begin{<>}
+				<>
+				\end{<>}
+			]],
+			{
+				i(1),
+				i(2),
+				rep(1)
+			}
+		)
+	),
+
 }
+
+return M
