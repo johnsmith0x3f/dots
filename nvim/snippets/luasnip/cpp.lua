c9c8e12ea83266d8d333cded73a5ad853cbed1b7
johnsmith0x3f <johnsmith0x3f@gmail.com>
2024-07-21 09:44:20 +0000

    Update neovim config.

---
+++ nvim/snippets/luasnip/cpp.lua
@@ -0,0 +1,13 @@
+local M = {
+	s("fastread", t(
+			"template<typename T = int>",
+			"inline T read() {",
+			"	T x = 0; bool f = 1; char c = getchar();",
+			"	while(isdigit(c)^1) f &= (c != 45), c = getchar();",
+			"	while(isdigit(c)) x = (x<<1) + (x<<3) + (c^48), c = getchar();",
+			"	return f ? x : -x;",
+			"}"
+	)),
+}
+
+return M
