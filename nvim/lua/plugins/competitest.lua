e5a12c6ff1a95d2dd81d163f695026bd0666b5f1
johnsmith0x3f <johnsmith0x3f@gmail.com>
2024-07-24 03:11:20 +0000

    Update neovim competitest plugin.

--- nvim/lua/plugins/competitest.lua
+++ nvim/lua/plugins/competitest.lua
@@ -1,10 +1,29 @@
 return {
-    "xeluxee/competitest.nvim", name = "CompetiTest",
-    config = function()
-        vim.keymap.set("n", "<F1>", ":CompetiTest add_testcase<CR>", {})
-        vim.keymap.set("n", "<F2>", ":CompetiTest edit_testcase ", {})
-        vim.keymap.set("n", "<F3>", ":CompetiTest delete_testcase ", {})
-        vim.keymap.set("n", "<F4>", ":CompetiTest run<CR>", {})
-        require("competitest").setup({})
-    end
+	name = "CompetiTest", "xeluxee/competitest.nvim",
+	
+	lazy = false,
+	keys = {
+		{ mode = "n", "<F5>", "<Cmd>CompetiTest run<CR>" },
+		{ mode = "n", "<F6>", "<Cmd>CompetiTest add_testcase<CR>" },
+		{ mode = "n", "<F7>", "<Cmd>CompetiTest edit_testcase<CR>" },
+		{ mode = "n", "<F8>", "<Cmd>CompetiTest delete_testcase<CR>" }
+	},
+
+	opts = {
+		runner_ui = {
+			interface = "popup"
+		},
+		compile_directory = ".",
+		compile_command = {
+			c = { exec = "gcc", args = { "-Wall", "$(FNAME)", "-o", "$(FNOEXT).out" } },
+			cpp = { exec = "g++", args = { "-Wall", "$(FNAME)", "-o", "$(FNOEXT).out" } }
+		},
+		running_directory = ".",
+		run_command = {
+			c = { exec = "./$(FNOEXT).out" },
+			cpp = { exec = "./$(FNOEXT).out" }
+		},
+		testcases_input_file_format = "$(FNOEXT)$(TCNUM).in",
+		testcases_output_file_format = "$(FNOEXT)$(TCNUM).ans"
+	}
 }
