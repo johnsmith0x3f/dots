3d4a2ccc34f35c830d16b14b9c115ca61bf78624
johnsmith0x3f <johnsmith0x3f@gmail.com>
2024-07-31 06:53:42 +0000

    Update polybar darktide theme.

--- .config/polybar/scripts/stalonetray.sh
+++ .config/polybar/scripts/stalonetray.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
+hidden="/tmp/stalonetray.hidden.lock"
+
 # Requires stalonetray.
-if [[ $(pidof stalonetray) ]]; then
-	polybar-msg action "#droptray.hook.0"
-	killall stalonetray
+if pgrep -x stalonetray; then
+	if [[ -e "$hidden" ]]; then
+		polybar-msg action "#droptray.hook.1"
+		xdo show -n stalonetray; rm "$hidden"
+	else
+		polybar-msg action "#droptray.hook.0"
+		xdo hide -n stalonetray; touch "$hidden"
+	fi
 else
 	polybar-msg action "#droptray.hook.1"
 	stalonetray --background=#313131 \
-	            --geometry=1x1+10+55 \
+	            --geometry=1x1+10+60 \
 	            --grow-gravity=W \
 	            --slot-size=32 \
 	            --window-type=toolbar &
