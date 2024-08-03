d9143ee40c620a271310bc7332129b403ef00f47
johnsmith0x3f <johnsmith0x3f@gmail.com>
2024-08-03 05:24:36 +0000

    Update polybar darktide theme.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-config="$XDG_CONFIG_HOME/polybar/themes/$1/config.ini"
+dir="$XDG_CONFIG_HOME/polybar/themes"
+config="themes/$1/config.ini"
 
 if [[ -f "$config" ]]; then
 	polybar-msg cmd quit
 	polybar -c "$config" &
 else
-	echo "Please specify one of the following themes:"
-	for theme in $XDG_CONFIG_HOME/polybar/themes/*/; do
-		theme="${theme%/}"
+	echo "Available themes:"
+	for theme in $dir/*/; do
+		theme="${theme%/*}"
 		echo "${theme##*/}"
 	done
 fi
