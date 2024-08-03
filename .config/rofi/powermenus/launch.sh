4d5d2923c4ec01203f9b111e9a05a0970bdaf4d1
johnsmith0x3f <johnsmith0x3f@gmail.com>
2024-08-03 04:46:37 +0000

    Update rofi config.

--- .config/rofi/powermenus/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-dir="${XDG_CONFIG_HOME:-$HOME/.config}/rofi/powermenus"
-choice="$(echo -e "\n\n" | rofi -dmenu -p "${USER}" -theme ${dir}/${1}.rasi)"
-
-case ${choice} in
-	"") betterlockscreen -l;;
-	  *) echo "${choice}";;
-esac
