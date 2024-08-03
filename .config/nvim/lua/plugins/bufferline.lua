a03aff1435d8bd9d67cb8fc2cb9a46187b6a4f64
johnsmith0x3f <johnsmith0x3f@gmail.com>
2024-08-03 07:27:16 +0000

    Update neovim config.

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,18 +1,19 @@
 return {
-    name = "BufferLine", "akinsho/bufferline.nvim",
+	name = "BufferLine", "akinsho/bufferline.nvim",
 
-    lazy = false,
-    keys = {
-        -- Goto buffer.
-        { mode = "n", "<Leader>gb", "<Cmd>BufferLinePick<CR>" }
-    },
+	lazy = false,
+	keys = {
+		-- Goto buffer.
+		{ mode = "n", "<Leader>gb", "<Cmd>BufferLinePick<CR>" }
+	},
 
-    config = function()
-        require("bufferline").setup({
-            options = {
-                indicator = { style = "none" },
-                separator_style = "slant"
-            }
-        })
-    end
+	opts = {
+		highlights = {
+			fill = { bg = "bg" }
+		},
+		options = {
+			indicator = { style = "icon" },
+			separator_style = "thin"
+		}
+	}
 }
