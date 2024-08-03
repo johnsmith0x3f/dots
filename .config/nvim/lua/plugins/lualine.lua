352b5dfc34d47ae368257c606b0fcf340b85a396
johnsmith0x3f <johnsmith0x3f@gmail.com>
2024-08-03 12:35:47 +0000

    Update neovim config.

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,16 +1,31 @@
 return {
-    name = "LuaLine", "nvim-lualine/lualine.nvim",
+	name = "LuaLine", "nvim-lualine/lualine.nvim",
 
-    config = function()
-        require("lualine").setup({
-            sections = {
-                lualine_a = { "", "mode" },
-                lualine_b = { "branch", "diff", "diagnostics" },
-                lualine_c = { "filename" },
-                lualine_x = { "filetype" },
-                lualine_y = { "progress" },
-                lualine_z = { "location" }
-            }
-        })
-    end
+	opts = {
+		options = {
+			component_separators = { left = "", right = "" },
+			secton_separators = { left = "", right = "" },
+			disabled_filetypes = {
+				statusline = {},
+				winbar = {}
+			},
+			ignore_focus = { "NvimTree" }
+		},
+		sections = {
+			lualine_a = {
+				{ "mode", separator = { left = "", right = "" } }
+			},
+			lualine_b = {
+				{ "branch", "diff", "diagnostics", separator = { right = "" } }
+			},
+			lualine_c = { "filename" },
+			lualine_x = { "filetype" },
+			lualine_y = {
+				{ "progress", separator = { left = "" } }
+			},
+			lualine_z = {
+				{ "location", separator = { left = "", right = "" } }
+			}
+		}
+	}
 }
