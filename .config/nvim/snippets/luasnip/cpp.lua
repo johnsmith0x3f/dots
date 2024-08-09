c797a52c5ec065187cced0589c162f1b335bf8b7
johnsmith0x3f <johnsmith0x3f@gmail.com>
2024-08-09 12:30:52 +0000

    Update cpp snippets.

--- .config/nvim/snippets/luasnip/cpp.lua
+++ .config/nvim/snippets/luasnip/cpp.lua
@@ -1,7 +1,7 @@
 local M = {
 	s("fastread", t({
-		"template<typename T = int>",
-		"inline T read() {",
+		"template <typename T = int>",
+		"constexpr T read() {",
 		"\tT x = 0; bool f = 1; char c = getchar();",
 		"\twhile(isdigit(c)^1) f &= (c != 45), c = getchar();",
 		"\twhile(isdigit(c)) x = (x<<1) + (x<<3) + (c^48), c = getchar();",
