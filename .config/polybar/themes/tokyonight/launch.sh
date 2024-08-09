1f4a660b3bfb550d01a587d8e2fdb4f51a01404d
johnsmith0x3f <johnsmith0x3f@gmail.com>
2024-08-09 02:52:51 +0000

    Update polybar tokyonight theme.

--- .config/polybar/themes/tokyonight/launch.sh
+++ .config/polybar/themes/tokyonight/launch.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+# Terminate running instances.
 polybar-msg cmd quit
 
 dir="${0%/*}"
 
-polybar -c "$dir/config.ini" ubar -q &
-sleep 0.1
+# The order matters.
 polybar -c "$dir/config.ini" mbar -q &
 polybar -c "$dir/config.ini" nbar -q &
 polybar -c "$dir/config.ini" rbar -q &
+
+sleep 0.1
+
+polybar -c "$dir/config.ini" ubar -q &
