b07879f2b70a3e38273fee72efed408c7b4d04d9
johnsmith0x3f <johnsmith0x3f@gmail.com>
2024-08-19 14:30:44 +0000

    Update bufferline keymaps.

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -4,7 +4,13 @@ return {
 	lazy = false,
 	keys = {
 		-- Goto buffer.
-		{ mode = "n", "<Leader>gb", "<Cmd>BufferLinePick<CR>" }
+		{ mode = "n", "[b", "<Cmd>BufferLineCyclePrev<CR>" },
+		{ mode = "n", "]b", "<Cmd>BufferLineCycleNext<CR>" },
+		{ mode = "n", "<Leader>1", "<Cmd>BufferLineGoToBuffer 1<CR>" },
+		{ mode = "n", "<Leader>2", "<Cmd>BufferLineGoToBuffer 2<CR>" },
+		{ mode = "n", "<Leader>3", "<Cmd>BufferLineGoToBuffer 3<CR>" },
+		{ mode = "n", "<Leader>4", "<Cmd>BufferLineGoToBuffer 4<CR>" },
+		{ mode = "n", "<Leader>5", "<Cmd>BufferLineGoToBuffer 5<CR>" }
 	},
 
 	opts = {
