aa0eac5abcfa0f2029d6fbdee12b94f8b3ea2840
johnsmith0x3f <johnsmith0x3f@gmail.com>
2024-09-26 08:09:01 +0000

    Add gruvbox colorscheme.

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,8 @@
 return {
+	-- Gruvbox
+	{ "ellisonleao/gruvbox.nvim", priority = 1000 },
 	-- One Dark Pro
 	{ "olimorris/onedarkpro.nvim", priority = 1000 },
 	-- Tokyo Night
-	{ "folke/tokyonight.nvim", priority = 1000 }
+	{ "folke/tokyonight.nvim", priority = 1001 }
 }
