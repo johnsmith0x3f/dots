4314df0c038d8a7d1ad2fd8b9781a8298b5aa59f
John Smith <johnsmith0x3f@gmail.com>
2024-10-10 03:24:19 +0000

    Add plenary.

--- .config/nvim/lua/plugins/lib.lua
+++ .config/nvim/lua/plugins/lib.lua
@@ -1,4 +1,6 @@
 return {
-	-- I want fancy icons!
-	{ "nvim-tree/nvim-web-devicons", lazy = true }
+	-- I need fancy icons.
+	{ "nvim-tree/nvim-web-devicons", lazy = true },
+	-- All the lua functions TJ don't want to write twice.
+	{ "nvim-lua/plenary.nvim", lazy = true }
 }
