e7d0ce29b91f86220291da835348ef2ef4e0738b
John Smith <johnsmith0x3f@gmail.com>
2024-10-10 09:14:27 +0000

    Add treesitter.

---
+++ .config/nvim/lua/plugins/language.lua
@@ -0,0 +1,32 @@
+return {
+	-- Treesitter
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = function()
+			require("nvim-treesitter.install").update({ with_sync = true })
+		end,
+		
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"bash",
+					"c",
+					"cpp",
+					"ini",
+					"lua",
+					"markdown",
+					"markdown_inline",
+					"vim",
+					"vimdoc"
+				},
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false
+				},
+				indent = {
+					enable = true
+				}
+			})
+		end
+	}
+}
