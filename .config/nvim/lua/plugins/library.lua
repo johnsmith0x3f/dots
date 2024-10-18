45253b7ec3dd6539272496023445e74266804d18
John Smith <johnsmith0x3f@gmail.com>
2024-10-18 15:19:46 +0000

    Add nvim-notify.

--- .config/nvim/lua/plugins/library.lua
+++ .config/nvim/lua/plugins/library.lua
@@ -4,5 +4,7 @@ return {
 	-- All the Lua functions TJ don't want to write twice.
 	{ "nvim-lua/plenary.nvim", lazy = true },
 	-- UI Component Library for Neovim.
-	{ "MunifTanjim/nui.nvim", lazy = true }
+	{ "MunifTanjim/nui.nvim", lazy = true },
+	-- A fancy, configurable, notification manager for NeoVim.
+	{ "rcarriga/nvim-notify", lazy = true, opts = { top_down = false } },
 }
