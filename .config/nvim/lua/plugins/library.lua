39dc59b1c809f34fa97abab12cfa7a5211c4f872
John Smith <johnsmith0x3f@gmail.com>
2024-10-18 15:11:11 +0000

    Add nui.

--- .config/nvim/lua/plugins/library.lua
+++ .config/nvim/lua/plugins/library.lua
@@ -1,6 +1,8 @@
 return {
-	-- I need fancy icons.
+	-- A Lua fork of vim-devicons.
 	{ "nvim-tree/nvim-web-devicons", lazy = true },
-	-- All the lua functions TJ don't want to write twice.
-	{ "nvim-lua/plenary.nvim", lazy = true }
+	-- All the Lua functions TJ don't want to write twice.
+	{ "nvim-lua/plenary.nvim", lazy = true },
+	-- UI Component Library for Neovim.
+	{ "MunifTanjim/nui.nvim", lazy = true }
 }
