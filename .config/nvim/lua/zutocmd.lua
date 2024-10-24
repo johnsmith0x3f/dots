af2e06a6396f71ec07b0e9068c20eaee8d085ad5
johnsmith0x3f <johnsmith0x3f@gmail.com>
2024-10-24 08:54:46 +0000

    Update autocmd.

--- .config/nvim/lua/zutocmd.lua
+++ .config/nvim/lua/zutocmd.lua
@@ -4,6 +4,6 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 	pattern = { "cpp" },
 	callback = function()
 		vim.opt.tabstop = 4
-		vim.keymap.set({ "n", "i" }, "<F5>", "<CMD>write <Bar> terminal g++ -std=c++17 -o %:r.out % && ./%:r.out<CR>")
+		vim.keymap.set({ "n", "i" }, "<F5>", "<CMD>write <Bar> terminal g++ -std=c++17 -o %:r.out % && %:r.out<CR>")
 	end
 })
