3afc17b70ae2eabb5a03c92db1e3151cccd8bc33
johnsmith0x3f <johnsmith0x3f@gmail.com>
2024-10-27 17:11:16 +0000

    Add which-key.

--- .config/nvim/lua/plugins/utility.lua
+++ .config/nvim/lua/plugins/utility.lua
@@ -60,5 +60,19 @@ return {
 				end
 			end, { silent = true })
 		end
+	},
+
+	-- Which Key
+	-- WhichKey helps you remember your Neovim keymaps, by showing available keybindings in a popup as you type.
+	{
+		"folke/which-key.nvim",
+
+		event = "VeryLazy",
+		keys = {
+			{ "<Leader>?", function() require("which-key").show({ global = false }) end }
+		},
+
+		opts = {
+		},
 	}
 }
