eb271955ae402dcc2e097e1d59a18fa050a96e05
John Smith <johnsmith0x3f@gmail.com>
2024-11-04 16:32:20 +0000

    Add dynamic resolution support.

--- .config/polybar/themes/tokyonight/scripts/stalonetray.sh
+++ .config/polybar/themes/tokyonight/scripts/stalonetray.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
 hidden="/tmp/stalonetray.hidden.lock"
+offset="$(($(xrandr -q | grep "primary" | cut -d " " -f 4 | cut -d "+" -f 1 | cut -d "x" -f 1) / 10 + 78))"
 
 # Requires stalonetray.
 if pgrep -x stalonetray; then
-	if [[ -e "$hidden" ]]; then
+	if [ -e "${hidden}" ]; then
 		polybar-msg action "#droptray.hook.1"
-		xdo show -n stalonetray; rm "$hidden"
+		xdo show -n stalonetray; rm "${hidden}"
 	else
 		polybar-msg action "#droptray.hook.0"
-		xdo hide -n stalonetray; touch "$hidden"
+		xdo hide -n stalonetray; touch "${hidden}"
 	fi
 else
 	polybar-msg action "#droptray.hook.1"
 	stalonetray --background="#24283B" \
-	            --geometry="1x1+270+60" \
+	            --geometry="1x1+${offset}+60" \
 	            --kludges="force_icons_size" \
 	            --slot-size=32 \
 	            --window-type="toolbar" \
