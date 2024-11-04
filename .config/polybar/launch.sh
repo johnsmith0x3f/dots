c8da89444da513eb864059e9425be1c3fe35b0f0
John Smith <johnsmith0x3f@gmail.com>
2024-11-04 16:13:22 +0000

    Fix error.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 dir="${XDG_CONFIG_HOME:-${HOME}/.config}/polybar/themes"
-script="$dir/${1}/lauhch.sh"
+script="$dir/${1}/launch.sh"
 
 if [[ -f "${script}" ]]; then
 	bash "${script}"
