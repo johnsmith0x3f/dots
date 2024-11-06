5c5c59ddc88dde9342da36b7d67f1b5cdd31d664
John Smith <johnsmith0x3f@gmail.com>
2024-11-06 13:18:02 +0000

    Hide stderr message.

--- .config/ncmpcpp/bin/cover.sh
+++ .config/ncmpcpp/bin/cover.sh
@@ -3,4 +3,4 @@
 cover="${HOME}/Music/$( ncmpcpp --current-song="%F" 2>/dev/null )"
 cover="${cover%/*}/$( ls "${cover%/*}" | grep -E "(cover|folder).(jpeg|jpg|png)" )"
 
-notify-send -i "${cover}" "Now Playing" "$( ncmpcpp --current-song="%t - %a" )"
+notify-send -i "${cover}" "Now Playing" "$( ncmpcpp --current-song="%t - %a" 2>/dev/null )"
