860f9ae441be912946023102f91c483f82c285f6
John Smith <johnsmith0x3f@gmail.com>
2024-11-12 08:38:37 +0000

    Add multi-resolution support.

--- .config/polybar/themes/tokyonight/launch.sh
+++ .config/polybar/themes/tokyonight/launch.sh
@@ -3,12 +3,19 @@
 # Terminate running instances.
 polybar-msg cmd quit
 
-dir="${0%/*}"
+# Support 1920x1080 and 2560x1440.
+width="$( xrandr -q | grep "primary" | cut -d " " -f 4 | cut -d "x" -f 1 )"
+if [[ $width -eq 1920 ]]; then
+	config="${0%/*}/config-1920x1080.ini"
+else
+	config="${0%/*}/config-2560x1440.ini"
+fi
+
 
 # The order matters.
-polybar -c "${dir}/config.ini" mbar -q &
-polybar -c "${dir}/config.ini" rbar -q &
+polybar -c "${config}" mbar -q &
+polybar -c "${config}" rbar -q &
 
 sleep 0.2
 
-polybar -c "${dir}/config.ini" ubar -q &
+polybar -c "${config}" ubar -q &
