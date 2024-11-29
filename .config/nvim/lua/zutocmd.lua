7586974e4f5beb6e07b19d5d051cad95b2f96f91
John Smith <johnsmith0x3f@gmail.com>
2024-11-29 15:40:44 +0000

    feat: keybind to compile-run python

--- .config/nvim/lua/zutocmd.lua
+++ .config/nvim/lua/zutocmd.lua
@@ -7,3 +7,12 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 		vim.keymap.set({ "n", "i" }, "<F5>", "<CMD>write <Bar> terminal g++ -std=c++17 -o %:r.out % && ./%:r.out<CR>")
 	end
 })
+
+-- Python
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = { "python" },
+	callback = function()
+		vim.opt.tabstop = 4
+		vim.keymap.set({ "n", "i" }, "<F5>", "<CMD>write <Bar> terminal python %<CR>")
+	end
+})
