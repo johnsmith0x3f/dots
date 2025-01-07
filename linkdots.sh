2a6db71152d836d249203bc0be6def24b8ba6f4d
John Smith <johnsmith0x3f@gmail.com>
2025-01-07 18:18:59 +0000

    feat: update stow script

--- linkdots.sh
+++ linkdots.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-# Stow etc (.config) and usr (.local) directory.
-stow -d ./etc -t ~/.config . && stow -d ./usr -t ~/.local .
+# Stow or unstow etc (.config) and usr (.local) directory.
+stow $1 -d ./etc -t ~/.config . && stow $1 -d ./usr -t ~/.local .
