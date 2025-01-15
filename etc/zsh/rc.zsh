2ce02f58429206828a134692d9e6a32a79609ae9
John Smith <johnsmith0x3f@gmail.com>
2025-01-15 04:24:44 +0000

    feat: add options

---
+++ etc/zsh/rc.zsh
@@ -0,0 +1,14 @@
+
+# Options {{{
+# https://zsh.sourceforge.io/Intro/intro_16.html
+
+# When there is an unmatched quote, report an error
+# instead of prompting for another line.
+setopt cshjunkiequotes
+
+# Do not save adjacent duplicate commands to $HISTFILE.
+setopt histignoredups
+# Do not save commands with leading spaces to $HISTFILE.
+setopt histignorespace
+
+# }}}
