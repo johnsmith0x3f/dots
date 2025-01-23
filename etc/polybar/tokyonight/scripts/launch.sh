6035f10bc60251f258aadca397479d7b034ce8e0
John Smith <johnsmith0x3f@gmail.com>
2025-01-23 13:21:40 +0000

    feat: update launch script

--- etc/polybar/tokyonight/scripts/launch.sh
+++ etc/polybar/tokyonight/scripts/launch.sh
@@ -4,18 +4,14 @@
 polybar-msg cmd quit
 
 # Support 1920x and 2560x.
-width="$( xrandr -q | grep "primary" | cut -d " " -f 4 | cut -d "x" -f 1 )"
+config="${0%/*}/../config-$( xrandr -q | grep "*" | grep -Eo "(1920|2560)" ).ini"
 
-if [[ $width -eq 1920 ]]; then
-	config="${0%/*}/../config-1920.ini"
-else
-	config="${0%/*}/../config-2560.ini"
-fi
+echo $config
 
-# The order matters.
-polybar -c "${config}" mbar -q &
-polybar -c "${config}" rbar -q &
-
-sleep 0.2
-
-polybar -c "${config}" ubar -q &
+## The order matters.
+#polybar -c "$config" mbar -q &
+#polybar -c "$config" rbar -q &
+#
+#sleep 0.2
+#
+#polybar -c "$config" ubar -q &
