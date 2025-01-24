5acf0cd15da59d0f2bb4fb7a19f5509febe32768
John Smith <johnsmith0x3f@gmail.com>
2025-01-24 06:48:41 +0000

    chore: rearrange files

---
+++ etc/polybar/tokyonight/launch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Terminate running instances.
+polybar-msg cmd quit
+
+# Support 1920x and 2560x.
+config="${0%/*}/config-$( xrandr -q | grep "*" | grep -Eo "(1920|2560)" ).ini"
+
+# The order matters.
+polybar -c "$config" mbar -q &
+polybar -c "$config" rbar -q &
+
+sleep 0.2
+
+polybar -c "$config" ubar -q &
