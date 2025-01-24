11db4c58310f6aca41d78a8add3a480a50c97a1e
John Smith <johnsmith0x3f@gmail.com>
2025-01-24 12:39:47 +0000

    fix: path issue

--- etc/polybar/tokyonight/launch.sh
+++ etc/polybar/tokyonight/launch.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-# Quit running instances.
+# Quit running instances using IPC command.
 polybar-msg cmd quit
 
-# Support 1920x and 2560x screens.
-config="${0%/*}/config-$( xrandr -q | grep "*" | grep -Eo "(1920|2560)" ).ini"
+# Pass the CWD to polybar.
+export dir="${0%/*}"
 
-# The order matters.
+# Use different config for different width.
+config="${dir}/config-$( xrandr -q | grep "*" | grep -Eo "(1920|2560)" ).ini"
+
+# Invoke the bars. Order matters.
 polybar -c "$config" mbar -q &
 polybar -c "$config" rbar -q &
-
 sleep 0.2
-
 polybar -c "$config" ubar -q &
