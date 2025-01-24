bc46152ff9bd5e03beb06ecb534ff78ad8d28d78
John Smith <johnsmith0x3f@gmail.com>
2025-01-24 10:01:25 +0000

    chore: rearrange files

--- etc/polybar/tokyonight/scripts/systray-1920.sh
+++ etc/polybar/tokyonight/scripts/systray-1920.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
-hidden="/tmp/stalonetray.hidden.lock"
+hidden="/tmp/stalonetray_hidden.lock"
 
-# Requires stalonetray.
-if pgrep -x stalonetray; then
-	if [[ -e "$hidden" ]]; then
+# Requires stalonetray & xdo.
+if pgrep -x stalonetray &>/dev/null; then
+	if [ -e "$hidden" ]; then
 		polybar-msg action "#droptray.hook.1"
 		xdo show -n stalonetray; rm "$hidden"
 	else
@@ -12,15 +12,9 @@ if pgrep -x stalonetray; then
 		xdo hide -n stalonetray; touch "$hidden"
 	fi
 else
-	if [[ $1 -eq 1920 ]]; then
-		dx=152
-	else
-		dx=184
-	fi
-
 	polybar-msg action "#droptray.hook.1"
 	stalonetray --background="#24283B" \
-	            --geometry="1x1+$1+64" \
+	            --geometry="1x1+152+64" \
 	            --kludges="force_icons_size" \
 	            --slot-size=32 \
 	            --window-type="toolbar" \
