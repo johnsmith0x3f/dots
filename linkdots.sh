00e53d388950588335e5d4b74a710ed2993a8ec5
John Smith <johnsmith0x3f@gmail.com>
2025-05-10 08:12:38 +0000

    feat: update script

--- linkdots.sh
+++ linkdots.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
+# TODO: Replace stow with a custom function.
+
 echo -e "\033[1;32mSyncing conf files...\033[0;39m"
 stow -nv -d ./etc -t "${XDG_CONFIG_HOME}" . 2>&1 | head -n -1
-echo -en "\x1B[1;32mContinue? [Y/n]\x1B[0;39m " && read answer && [[ "$answer" != "n"* ]] && stow -d ./etc -t "${XDG_CONFIG_HOME}" .
+echo -en "\033[1;32mContinue? [Y/n]\033[0;39m " && read answer && [[ "$answer" != "n"* ]] && stow -d ./etc -t "${XDG_CONFIG_HOME}" .
 echo
 
-echo -e "Syncing data files...\x1B[0;39m"
+echo -e "\033[1;32mSyncing data files...\033[0;39m"
 stow -nv -d ./usr -t "${XDG_DATA_HOME%/*}" . 2>&1 | head -n -1
-echo -en "\x1B[1;32mContinue? [Y/n]\x1B[0;39m " && read answer && [[ "$answer" != "n"* ]] && stow -d ./usr -t "${XDG_DATA_HOME%/*}" .
+echo -en "\033[1;32mContinue? [Y/n]\033[0;39m " && read answer && [[ "$answer" != "n"* ]] && stow -d ./usr -t "${XDG_DATA_HOME%/*}" .
 echo
 
-echo -e "Syncing root files...\x1B[0;39m"
+echo -e "\033[1;32mSyncing root files...\033[0;39m"
 stow -nv -d ./@ -t / . 2>&1 | head -n -1
-echo -en "\x1B[1;32mContinue? [Y/n]\x1B[0;39m " && read answer && [[ "$answer" != "n"* ]] && sudo stow -d ./@ -t / .
+echo -en "\033[1;32mContinue? [Y/n]\033[0;39m " && read answer && [[ "$answer" != "n"* ]] && sudo stow -d ./@ -t / .
 
 unset answer
