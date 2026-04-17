86e5f425b0c745ecf49b7af1fc8733fd3938111e
John Smith <johnsmith0x3f@gmail.com>
2026-04-17 13:35:56 +0000

    feat: update alias, add yazi function

--- etc/zsh/rc.zsh
+++ etc/zsh/rc.zsh
@@ -8,41 +8,22 @@ bindkey -M vicmd v edit-command-line
 # See https://zsh.sourceforge.io/Doc/Release/Shell-Grammar.html#Aliasing for details.
 
 # I like better commands.
-alias cat="bat --plain --paging=never"
-alias ls="eza --icons=auto --group-directories-first"
+alias ls="ls --classify --color --group-directories-first --human-readable"
 alias grep="grep --color=auto"
+
+alias b="bat --plain --paging=never"
+alias e="eza --icons=auto	--group-directories-first"
 
 # I fetch a lot.
 alias f="fastfetch"
 
 # I use lazygit.
 alias g="lazygit"
-# I also use plain git.
-alias ga="git add" # my barbaric yawp over the files of the commits
-alias gaa="git add --all" # yawp harder to commit all the files
-alias gb="git branch"
-alias gbc="git checkout -b" # branch then checkout
-alias gc="git commit"
-alias gca="git commit --amend"
-alias gf="git fetch"
-alias gpl="git pull"
-alias gps="git push"
-alias grm="git remote"
-alias grma="git remote add"
-alias grmr="git remote remove"
-alias grmv="git remote rename"
-alias grms="git remote set-url"
-alias grs="git restore"
-alias grss="git restore --staged"
-alias gs="git status"
 
 # I use nvim btw.
 alias v="nvim"
 alias vi="nvim"
 alias vim="nvim"
-
-# I use Zed (sometimes) btw.
-alias zed="zeditor"
 
 # }}}
 
